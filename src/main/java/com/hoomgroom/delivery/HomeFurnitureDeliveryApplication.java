740a92cff728f32be6fd88a49c84e1c5b66d45ad
RyanAfzal <ryanafzal3@gmail.com>
2024-04-06 15:25:54 +0700

    [INIT] Initiate new SpringBoot project

---
+++ src/main/java/com/hoomgroom/delivery/HomeFurnitureDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.hoomgroom.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeFurnitureDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeFurnitureDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hoomgroom/delivery/HomeFurnitureDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoomgroom.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFurnitureDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
