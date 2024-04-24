fe118b685770df4514f20f906544151026d02da2
ryandhika_al <ryandhika.al@ui.ac.id>
2024-04-24 11:09:29 +0700

    [REFACTOR] fix typo in test

--- src/test/java/com/hoomgroom/delivery/model/DeliveryTest.java
+++ src/test/java/com/hoomgroom/delivery/model/DeliveryTest.java
@@ -1,34 +1,32 @@
+package com.hoomgroom.delivery.model;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DeliveryTest {
+    private Transportation transportation;
+    private Delivery delivery;
+
+    @BeforeEach
+    void setUp() {
+        this.transportation = new Transportation("Truck");
+        this.delivery = new Delivery(DeliveryStatus.MENUNGGU_VERIFIKASI, transportation, "ABC123");
+    }
+
     @Test
-    public void testModelDelivery() {
-        Transportation transportation;
-        Delivery delivery;
+    void testGetDeliveryStatus() {
+        assertEquals(DeliveryStatus.MENUNGGU_VERIFIKASI, delivery.getStatus());
+    }
 
-        @BeforeEach
-        void setUp(){
-            this.transportation = new Transportation();
-            this.transportation.setType("Truck");
-            this.delivery = new ModelDelivery(DeliveryStatus.MENUNGGU_VERIFIKASI, transportation, "ABC123");
-        }
+    @Test
+    void testGetTransportation() {
+        assertEquals("Truck", delivery.getTransportation().getType());
+    }
 
-        @Test
-        void testGetDeliveryStatus(){
-            assertEquals(DeliveryStatus.MENUNGGU_VERIFIKASI, delivery.getStatus());
-        }
-
-        @Test
-        void testGetTransportation(){
-            assertEquals("Truck", delivery.getTransportation().getType());
-        }
-
-        @Test
-        void testGetKoderesi(){
-            assertEquals("ABC123", delivery.getKodeResi());
-        }
+    @Test
+    void testGetKodeResi() {
+        assertEquals("ABC123", delivery.getKodeResi());
     }
 }
