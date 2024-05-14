81e51cac0af976619f485bd08f71779f99088362
ryandhika_al <ryandhika.al@ui.ac.id>
2024-05-14 22:11:04 +0700

    [REFACTOR] kodeResi can be generated using UUID

--- src/main/java/com/hoomgroom/delivery/model/Delivery.java
+++ src/main/java/com/hoomgroom/delivery/model/Delivery.java
@@ -1,5 +1,7 @@
 package com.hoomgroom.delivery.model;
 
+import java.util.UUID;
+
 import com.hoomgroom.delivery.enums.DeliveryStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,12 @@ public class Delivery {
 
     @Id 
     private String kodeResi;
+
+    public void setKoderesi(){
+        UUID kodeResiUUID = UUID.randomUUID();
+        String kodeResiGenerated = kodeResiUUID.toString();
+        this.kodeResi = kodeResiGenerated;
+    }
 
     @Enumerated(EnumType.STRING)
     private DeliveryStatus status = DeliveryStatus.MENUNGGU_VERIFIKASI;
